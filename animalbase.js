e8f8920d1c25b740e916dc2f2e7bcef632f2a04c
MathildeSahlholdt <math38j8@stud.kea.dk>
2021-09-20 11:23:34 +0200

    animalbase sorting with name and type and usability

---
+++ animalbase.js
@@ -0,0 +1,150 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", start);
+
+// creates an empty array
+let allAnimals = [];
+
+// The prototype for all animals: 
+const Animal = {
+    name: "",
+    desc: "-unknown animal-",
+    type: "",
+    age: 0
+};
+
+function start( ) {
+    console.log("ready");
+
+    // Add event-listeners to filter and sort buttons
+
+    registerButtons();
+
+    loadJSON();
+}
+
+function registerButtons() {
+    // for each button you click go to function selectFilter
+    document.querySelectorAll("[data-action='filter']")
+    .forEach(button => button.addEventListener("click", selectFilter));
+
+    document.querySelectorAll("[data-action='sort']")
+    .forEach(button => button.addEventListener("click", selectSort));
+}
+
+
+async function loadJSON() {
+    const response = await fetch("animals.json");
+    const jsonData = await response.json();
+    
+    // when loaded, prepare data objects
+    prepareObjects( jsonData );
+}
+
+function prepareObjects( jsonData ) {
+    allAnimals = jsonData.map( preapareObject );
+
+    // TODO: This might not be the function we want to call first
+    displayList(allAnimals);
+}
+
+function preapareObject( jsonObject ) {
+    const animal = Object.create(Animal);
+    
+    const texts = jsonObject.fullname.split(" ");
+    animal.name = texts[0];
+    animal.desc = texts[2];
+    animal.type = texts[3];
+    animal.age = jsonObject.age;
+
+    return animal;
+}
+
+function selectFilter(event) {
+    // filter by the button that has been clicked
+    const filter = event.target.dataset.filter;
+    console.log(`User selected ${filter}`);
+    filterList(filter);
+}
+
+function filterList(filterBy) {
+    let filteredList = allAnimals;
+    // if the filter is set to cat, dog or all it filters it by that
+    if (filterBy === "cat") {
+        //create a filtered list of only cats
+        filteredList = allAnimals.filter(isCat);
+    } else if (filterBy === "dog"){
+        //create a filtered list of only dogs
+        filteredList = allAnimals.filter(isDog);
+    }
+    displayList(filteredList);
+}
+
+function isCat(animal) {
+    //returns the ones with type cat
+    return animal.type === "cat";
+}
+
+function isDog(animal) {
+    //returns the ones with type dog
+    return animal.type === "dog";
+}
+
+function selectSort(event) {
+    // filter by the button that has been clicked
+    const sortBy = event.target.dataset.sort;
+    console.log(`User selected ${sortBy}`);
+    sortList(sortBy);
+}
+
+function sortList(sortBy) {
+    let sortedList = allAnimals;
+
+    if (sortBy === "name") {
+        sortedList = sortedList.sort(sortByName);
+    } else if (sortBy === "type") {
+        sortedList = sortedList.sort(sortByType);
+    }
+
+    displayList(sortedList);
+}
+
+function sortByName(animalA, animalB) {
+    if (animalA.name < animalB.name) {
+        return -1;
+    } else {
+        return 1;
+    }
+}
+
+function sortByType(animalA, animalB) {
+    if (animalA.type < animalB.type) {
+        return -1;
+    } else {
+        return 1;
+    }
+}
+
+function displayList(animals) {
+    // clear the list
+    document.querySelector("#list tbody").innerHTML = "";
+
+    // build a new list
+    animals.forEach( displayAnimal );
+}
+
+function displayAnimal( animal ) {
+    // create clone
+    const clone = document.querySelector("template#animal").content.cloneNode(true);
+
+    // set clone data
+    clone.querySelector("[data-field=name]").textContent = animal.name;
+    clone.querySelector("[data-field=desc]").textContent = animal.desc;
+    clone.querySelector("[data-field=type]").textContent = animal.type;
+    clone.querySelector("[data-field=age]").textContent = animal.age;
+
+    // append clone to list
+    document.querySelector("#list tbody").appendChild( clone );
+}
+
+
